180765c557112c3deba4fd8672912942ea0412da
lsn <2061683799@qq.com>
2024-11-05 23:16:14 -0800

    add print file infomation method

--- src/main.c
+++ src/main.c
@@ -23,6 +23,11 @@ void print_devices_infomation() {
         char *friendlyname;
         char *serialnumber;
 
+        char *vendor = devices[i].device_entry.vendor;
+        char *product = devices[i].device_entry.product;
+
+        printf("vendor: %s, product: %s\n", vendor, product);
+
         device = LIBMTP_Open_Raw_Device_Uncached(&devices[i]);
         if (device == NULL) {
             fprintf(stderr, "Unable to open raw device %d\n", i);
@@ -37,8 +42,44 @@ void print_devices_infomation() {
             free(friendlyname);
         }
         free(serialnumber);
-        
+        LIBMTP_Release_Device(device);
     }
+    free(devices);
+}
+void print_flie_info() {
+    LIBMTP_raw_device_t *devices;
+    int device_num;
+
+    LIBMTP_error_number_t err = LIBMTP_Detect_Raw_Devices(&devices, &device_num);
+    print_error(err);
+
+    for (int i = 0; i < device_num; i++) {
+        LIBMTP_mtpdevice_t *device;
+        LIBMTP_devicestorage_t *storage;
+
+        device = LIBMTP_Open_Raw_Device(&device[i]);
+
+        for (storage = device->storage; storage != 0; storage = storage->next) {
+            LIBMTP_file_t *files;
+            files = LIBMTP_Get_Files_And_Folders(device,storage->id,LIBMTP_FILES_AND_FOLDERS_ROOT);
+            
+            while (files != NULL) {
+                LIBMTP_file_t *tmp;
+                printf("File ID: %u\n", files->item_id);
+                printf("Parent ID: %u\n", files->parent_id);
+                printf("Storage ID: %u\n", files->storage_id);
+                printf("File Type: %d\n", files->filetype);
+                printf("File Name: %s\n", files->filename);
+                printf("File Size: %llu bytes\n", (unsigned long long)files->filesize);
+                printf("-----------------------------------\n");
+                tmp = files;
+                files = files->next;
+                LIBMTP_destroy_file_t(tmp);
+            }
+        }
+        LIBMTP_Release_Device(device);
+    }
+    free(devices);
 }
 int print_error(LIBMTP_error_number_t err) {
     switch(err)
@@ -55,8 +96,7 @@ int print_error(LIBMTP_error_number_t err) {
         /* Unknown general errors - This should never execute */
         case LIBMTP_ERROR_GENERAL:
         default:
-            fprintf(stderr, "mtp-files: Unknown error, please report "
-                            "this to the libmtp developers\n");
+            fprintf(stderr, "mtp-files: Unknown error, please report this to the libmtp developers\n");
             return 1;
 
         /* Successfully connected at least one device, so continue */
